223fade26f2bc79d15edb5704c38cc53dfb0667e
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2023-04-23 12:31:01 -0700

    Updated Version string and description.

--- HandheldScannerImprovements/Mod.cs
+++ HandheldScannerImprovements/Mod.cs
@@ -5,13 +5,13 @@ namespace HandheldScannerImprovements
 {
     public class Mod : PulsarMod, IKeybind
     {
-        public override string Version => "0.0.1";
+        public override string Version => "1.0.0";
 
         public override string Author => "Dragon";
 
         public override string Name => "Handheld Scanner Improvements";
 
-        public override string LongDescription => "";
+        public override string LongDescription => "Provides improvements to the handheld scanner.\n- Adds fire detection to the scanner\n- Scanner levels effect the scanner range\n- Scanner colors can be customized\n- Adds keybind for toggling the scanner UI\n- Scanner no longer needs to be equipped on the hotbar to appear in the UI";
 
         public override string HarmonyIdentifier()
         {
